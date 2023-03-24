bf5acc4122462d09a28e9a2d279207621d6412c5
PedroLopes7K <pedrohenri3601@gmail.com>
2023-03-24 07:50:15 -0300

    Design Pattern Singleton

---
+++ Singleton/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../autoloader.php');
+
+use Singleton\LogsSingleton;
+
+$logsSingleton = LogsSingleton::obterInstancia();
+
+$logsSingleton->gravarLog(['test33']);
+
+$logsSingletonTeste = LogsSingleton::obterInstancia();
+
+if ($logsSingletonTeste === $logsSingleton) {
+    echo 'As instâncias são exatamente as mesmas!';
+}
+
+echo '<br>';
+echo 'Rodou tudo';
