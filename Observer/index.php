dccb38c015f9114da6ad0706760c9d094d7e9cf8
PedroLopes7K <pedrohenri3601@gmail.com>
2023-03-30 22:15:33 -0300

    Design Pattern Observer

---
+++ Observer/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('../autoloader.php');
+
+$controleEstoqueSubject = new \Observer\ControleEstoqueSubject();
+
+$controleRequisicaoCompraObserver = new \Observer\ControleRequisicoesCompraObserver();
+$emailObserver = new \Observer\EmailObserver();
+
+$controleEstoqueSubject->adicionarObserver($controleRequisicaoCompraObserver);
+$controleEstoqueSubject->adicionarObserver($emailObserver);
+
+// $controleEstoqueSubject->notificarObservers(123);
+$controleEstoqueSubject->atualizaEstoqueProduto('123',0);
