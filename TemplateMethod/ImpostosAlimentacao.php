2beea1a9acfd5cca6d16810798956fce6a96e2df
PedroLopes7K <pedrohenri3601@gmail.com>
2023-04-01 20:07:08 -0300

    Design Pattern Template Method

---
+++ TemplateMethod/ImpostosAlimentacao.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TemplateMethod;
+
+/**
+ * Class ImpostosAlimentacao
+ *
+ * @package TemplateMethod
+ *
+ */
+class ImpostosAlimentacao extends ImpostosAbstract
+{
+    /**
+     * @param float $precoProduto
+     *
+     * @return float
+     */
+    protected function calculaImpostoVariavel(float $precoProduto): float
+    {
+        return $precoProduto;
+    }
+}
